4b7b807a7f975e45f637fd6ad75676917e1fa17a
asadalikanwal <asad@indivirtualdubai.com>
2017-07-17 21:26:05 +0400

    changing parser

--- web/index.php
+++ web/index.php
@@ -51,39 +51,9 @@ $app->get('/getData', function() use($app) {
     }
 
 
-    function xml2js($xmlnode) {
-        $root = (func_num_args() > 1 ? false : true);
-        $jsnode = array();
-
-        if (!$root) {
-            if (count($xmlnode->attributes()) > 0){
-                $jsnode["$"] = array();
-                foreach($xmlnode->attributes() as $key => $value)
-                    $jsnode["$"][$key] = (string)$value;
-            }
-
-            $textcontent = trim((string)$xmlnode);
-            if (count($textcontent) > 0)
-                $jsnode["_"] = $textcontent;
-
-            foreach ($xmlnode->children() as $childxmlnode) {
-                $childname = $childxmlnode->getName();
-                if (!array_key_exists($childname, $jsnode))
-                    $jsnode[$childname] = array();
-                array_push($jsnode[$childname], xml2js($childxmlnode, true));
-            }
-            return $jsnode;
-        } else {
-            $nodename = $xmlnode->getName();
-            $jsnode[$nodename] = array();
-            array_push($jsnode[$nodename], xml2js($xmlnode, true));
-            return json_encode($jsnode);
-        }
-    }   
 
   $url = 'http://pf.tradetracker.net/?aid=1&fid=251713&categoryType=2&additionalType=2&limit=10';
-  // print XmlToJson::Parse($url);  
-  print xml2js($url);
+  print XmlToJson::Parse($url);  
 });
 
 $app->run();
