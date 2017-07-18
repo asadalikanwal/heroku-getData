67f3ce5359e8b364c6b07f3c9a4c9d879e35e5a2
asadalikanwal <asad@indivirtualdubai.com>
2017-07-18 09:22:08 +0400

    Updates

--- web/index.php
+++ web/index.php
@@ -152,7 +152,8 @@ $app->post('/getData', function() use($app) {
   
   // $url = 'http://pf.tradetracker.net/?aid=1&fid=251713&categoryType=2&additionalType=2&limit=10';
   // return XmlToJson::Parse($url);  
-  return xmlToArray($url);
+  $fileContents= file_get_contents($url);
+  return xmlToArray($fileContents);
 });
 
 $app->run();
