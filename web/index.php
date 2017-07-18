939b620cde2a92cf0e105fa612b71f8c0c0a1cf1
asadalikanwal <asad@indivirtualdubai.com>
2017-07-18 12:13:40 +0400

    Updates

--- web/index.php
+++ web/index.php
@@ -66,7 +66,7 @@ $app->post('/getData', function() use($app) {
         // $fileContents = str_replace(array("\/"), '/', $fileContents);
         $fileContents = trim(str_replace('"', "'", $fileContents));
         $simpleXml = simplexml_load_string($fileContents);
-        $jsonFinal = str_replace(("\/"), '/', $simpleXml); 
+        $jsonFinal = str_replace(('\/'), '/', $simpleXml); 
         $json = json_encode($jsonFinal);
         return $json;
       } 
